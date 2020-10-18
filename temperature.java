f31b4423c3608ba06fdcfcd13a88700048b59308
0xRar <33517160+0xRar@users.noreply.github.com>
2020-10-18 07:18:11 +0300

    Create temperature.java

---
+++ temperature.java
@@ -0,0 +1,34 @@
+package com.company;
+
+/*
+Write a Java program that will predict tomorrow’s temperature given today’s temperature readings. The program
+will first ask the user to enter two values: the temperature median value and current temperature reading. The
+program will calculate and display the expected temperature according to the following equation:
+
+Expected temperature = 2 × median temperature − current temperature
+
+Format your output to two decimal places.
+*/
+
+
+import java.util.Scanner;
+public class temperature {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double TMV,ExpectedTemp,currentTemp;
+
+        System.out.print("Enter temperature median value: ");
+        TMV = input.nextDouble();
+
+        System.out.print("Enter current temperature reading: ");
+        currentTemp = input.nextDouble();
+
+        ExpectedTemp = 2 * TMV - currentTemp;
+
+        System.out.print("Expected temperature: "+ ExpectedTemp);
+
+//        Isa Ebrahim(0xRar)
+    }
+}
