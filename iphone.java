173b8a9f12fb9b61db01e2d6ca06df41bf9a51f8
0xRar <33517160+0xRar@users.noreply.github.com>
2020-10-25 18:44:43 +0300

    Update iphone.java

--- iphone.java
+++ iphone.java
@@ -1,5 +1,22 @@
 package com.company;
 
+
+/*
+An electronics store sells Apple iPhone XR in payments over a year,
+two, three, or four years to customers. Write a program asking to enter
+the name of the customer (string) and the price of the Apple iPhone XR (double),
+and then print a proposed schedule for the payments that the customer
+will pay monthly during one, two , three and four years.
+(Assuming there is no interest rate).
+
+
+monthly payment for one year=price ÷12
+monthly payment for two years=price ÷(12 x 2)
+monthly payment for three years=price ÷(12 x 3)
+monthly payment for four years=price ÷(12 x 4)
+
+*/
+
 import java.util.Scanner;
 
 public class iphone {
