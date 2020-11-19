279f39b6f8e7b4849361953027b272bd956ae3f2
0xRar <33517160+0xRar@users.noreply.github.com>
2020-11-19 17:20:44 +0300

    Create fertilizer.java

---
+++ fertilizer.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// IMPORTANT::: THIS PROBLEM WAS FROM TEST NUMBER 1.
+
+public class fertilizerTEST {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        float backyard, bagsize, bagPrice,NumOfBags,Total;
+
+        System.out.print("Enter backyard area: ");
+        backyard = input.nextFloat();
+
+        System.out.print("Enter fertilizer bag size: ");
+        bagsize = input.nextFloat();
+
+        System.out.print("Enter fertilizer bag price: ");
+        bagPrice = input.nextFloat();
+
+        NumOfBags =  backyard / (bagsize-4) + 1;
+        System.out.println("Number of bags = " + (int)NumOfBags);
+
+        Total = (int) NumOfBags * bagPrice;
+        System.out.println("Total price = " + Total + " BD.");
+
+        // Isa Ebrahim(0xRar)
+    }
+}
