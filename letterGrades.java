0f9252c3f3e8045d53e82f6ac79c56334c9f8d72
0xRar <33517160+0xRar@users.noreply.github.com>
2020-11-19 17:08:30 +0300

    Create letterGrades.java

---
+++ letterGrades.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class letterGrades {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char Grade;
+
+        System.out.print("Enter your grade: ");
+        Grade = input.nextLine().charAt(0);
+
+        if (Grade == 'E' || Grade == 'e') {
+            System.out.println("Your grade description is Excellent");
+
+        } else if (Grade == 'V' || Grade == 'v') {
+            System.out.println("Your grade description is Very Good");
+
+        } else if (Grade == 'G' || Grade == 'g') {
+            System.out.println("Your grade description is Good");
+
+        } else if (Grade == 'A' || Grade == 'a') {
+
+            System.out.println("Your grade description is Average");
+
+        } else if (Grade == 'F' || Grade == 'f') {
+            System.out.println("Your grade description is Fail");
+
+        } else {
+            System.out.println("Something went wrong!");
+        }
+//        Isa Ebrahim(0xRar)
+    }
+}
