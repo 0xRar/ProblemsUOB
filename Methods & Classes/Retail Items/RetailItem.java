d1f7f4ba38d82592f7233fa4cc27ef81392d1724
0xRar <33517160+0xRar@users.noreply.github.com>
2020-12-25 05:38:31 +0300

    Create RetailItem.java

---
+++ Methods & Classes/Retail Items/RetailItem.java
@@ -0,0 +1,31 @@
+public class RetailItem {
+    String Description = new String();
+    private int unitsOnHold;
+    private double price;
+
+//    setters
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public void setUnitsOnHold(int unitsOnHold) {
+        this.unitsOnHold = unitsOnHold;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+//    getters
+    public String getDescription() {
+        return Description;
+    }
+
+    public int getUnitsOnHold() {
+        return unitsOnHold;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
